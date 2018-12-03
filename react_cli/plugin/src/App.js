9809841a6260caba6de47075d2745d2c4e1440a4
IBEGHOUCHENE <ibeghouchene_nadir@yahoo.fr>
2018-12-03 13:48:51 +0100

    Premier affichage des noms de plugins

--- react_cli/plugin/src/App.js
+++ react_cli/plugin/src/App.js
@@ -1,28 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+      
+    this.state = {
+      plugins:[],
+      maxPag:0,
+      nbPlugi:0
+    }
+    this.update = this.update.bind(this);
+  }
+  
+	update(event){
+    let target = event.target;
+    let name = target.name;
+    let value = target.value;
+    this.setState({
+      [name] : value
+    });
+  }
+ 
+  getDataFromServer() {
+     fetch('http://localhost:8080/api/plugins')
+     .then(response => {
+       return response.json() // transforme le json texte en objet js
+     })
+     .then(res => { // data c'est le texte json de response ci-dessus
+       let plugins = [];
+       res.data.forEach((el) => {
+         let plugin = {
+            name:el.name,
+          }
+         plugins.push(plugin);
+       });
+       this.setState({
+        plugins: plugins
+      });
+       
+     }).catch(err => {
+       console.log("erreur dans le get : " + err)
+     });
+  }
+
+  getMaxPage(){
+    fetch("http://localhost:8080/api/plugin/count")
+    .then(response => {
+      return response.json();
+    }).then(resp => {
+      let maxpagevalue = Math.ceil(resp.data/this.state.nbPlugin)-1;
+      this.setState({maxPage: maxpagevalue, page:maxpagevalue});
+
+    }).catch(err => {
+      console.log("erreur dans le get : " + err)
+    });
+  }
+  
+  componentWillMount() {
+    console.log("Component will mount");
+    this.getDataFromServer();
+  }
+ 
   render() {
+
+    let listePlugins = this.state.plugins.map( (plugin, index) => {
+      return(
+        <tr v-for="(plugin,index) in filteredplugins" key={index}>
+          <td>{plugin.name}</td>
+          <td>
+          </td>
+        </tr>
+      )}
+    );
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div id="app">
+      <div id="entete">
+              Nombre de plugins : {this.state.plugins.length} 
+              </div>
+                <table  id="myTable" >
+                <thead >
+                <tr>
+                    <th><p>Nom</p></th>
+                </tr>
+                </thead>
+                <tbody>
+                   {
+                     listePlugins
+                   }
+                </tbody>
+                </table>
+              </div>
+      
     );
   }
 }
-
 export default App;
+
+
+
